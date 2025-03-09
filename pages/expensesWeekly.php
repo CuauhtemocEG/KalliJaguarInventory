a3d60e15e1d7a1f33785b21bad7f1bbd79dd6e16
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-09 00:05:28 -0600

    commit

--- pages/expensesWeekly.php
+++ pages/expensesWeekly.php
@@ -95,34 +95,3 @@ $totalGastos = calcularTotal($gastos);
         </div>
     </div>
 </div>
-<?php
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    $elminarExpense = conexion();
-    $elminarExpense = $elminarExpense->prepare("DELETE FROM Gastos WHERE ID=:id");
-
-    $elminarExpense->execute([":id" => $id]);
-
-    if ($elminarExpense->rowCount() == 1) {
-        echo '
-			<div class="alert alert-info alert-dismissible fade show" role="alert">
-            	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                	<span aria-hidden="true">&times;</span>
-         		</button>
-            	<strong>Gasto Eliminada!</strong><br>
-				Los datos del gasto se eliminaron con éxito.
-        	</div>';
-    } else {
-        echo '
-			<div class="alert alert-danger alert-dismissible fade show" role="alert">
-            	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                	<span aria-hidden="true">&times;</span>
-         		</button>
-            	<strong>¡Ocurrio un error!</strong><br>
-				No pudimos eliminar el gasto, por favor intente nuevamente.
-        	</div>';
-    }
-    $elminarExpense = null;
-}
-?>
