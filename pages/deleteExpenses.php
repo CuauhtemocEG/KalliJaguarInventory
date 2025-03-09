dd6e9f9e9fe197ad3a388960fd118e1abb830324
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-09 00:12:23 -0600

    commit

--- pages/deleteExpenses.php
+++ pages/deleteExpenses.php
@@ -1,17 +1,17 @@
 <?php
 if (isset($_GET['expensesId'])) {
-    $id = $_GET['expensesId'];
+	$id = $_GET['expensesId'];
 
-    $elminarExpense = conexion();
-    $elminarExpense = $elminarExpense->prepare("DELETE FROM Gastos WHERE ID=:id");
+	$elminarExpense = conexion();
+	$elminarExpense = $elminarExpense->prepare("DELETE FROM Gastos WHERE ID=:id");
 
-    $elminarExpense->execute([":id" => $id]);
+	$elminarExpense->execute([":id" => $id]);
 
-    if ($elminarExpense->rowCount() == 1) {
-        echo "<script>window.setTimeout(function() { window.location = 'index.php?page=expensesWeekly' }, 100);</script>";
-exit();
-    } else {
-        echo '
+	if ($elminarExpense->rowCount() == 1) {
+		echo "<script>window.setTimeout(function() { window.location = 'index.php?page=expensesWeekly' }, 100);</script>";
+		exit();
+	} else {
+		echo '
 			<div class="alert alert-danger alert-dismissible fade show" role="alert">
             	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 	<span aria-hidden="true">&times;</span>
@@ -19,6 +19,6 @@ exit();
             	<strong>¡Ocurrio un error!</strong><br>
 				No pudimos eliminar el gasto, por favor intente nuevamente.
         	</div>';
-    }
-    $elminarExpense = null;
+	}
+	$elminarExpense = null;
 }
