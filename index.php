0bfd4b9409fc8fc6e5d204b7b9d2fb9c4c65d79d
CuauhtemocEG <88246306+CuauhtemocEG@users.noreply.github.com>
2025-03-11 01:59:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <html>
 
 <head>
-    <?php include "./includes/header.php"; ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Kalli Jaguar Inventory</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
 <body class="">
@@ -20,7 +28,7 @@
         }
 
         if ($_SESSION['rol'] == "Administrador") {
-            include "./includes/navbarAdmin.php";
+            include "./includes/navbarNewTop.php";
         } elseif ($_SESSION['rol'] == "Supervisor") {
             include "./includes/navbarSup.php";
         } elseif ($_SESSION['rol'] == "Logistica") {
@@ -31,9 +39,18 @@
 
         include "./pages/" . $_GET['page'] . ".php";
 
-        //require_once "./includes/footer.php";
-
         include "./includes/script.php";
+
+        if ($_SESSION['rol'] == "Administrador") {
+            include "./includes/navbarNewBottom.php";
+        } elseif ($_SESSION['rol'] == "Supervisor") {
+            include "./includes/navbarSup.php";
+        } elseif ($_SESSION['rol'] == "Logistica") {
+            include "./includes/navbarLog.php";
+        } else {
+            echo "";
+        }
+        
     } else {
         if ($_GET['page'] == "login") {
             include "./pages/login.php";
@@ -42,5 +59,10 @@
         }
     }
     ?>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/sb-admin-2.min.js"></script>
 </body>
+
 </html>
