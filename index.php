0aac63a1d6af352f9ab0e7ea85dfa9fe6183f7df
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-11 02:59:51 -0600

    Change icon

--- index.php
+++ index.php
@@ -9,14 +9,12 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Kalli Jaguar Inventory</title>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
-<body class="">
+<body id="page-top">
     <?php
     if (!isset($_GET['page']) || $_GET['page'] == "") {
         $_GET['page'] = "login";
