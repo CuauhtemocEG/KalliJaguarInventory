15d8d5441a50815aef04c4ba8b2d5c6934e8e5bf
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-12 17:39:26 -0600

    Save document to request

--- pages/confirmRequest.php
+++ pages/confirmRequest.php
@@ -48,7 +48,7 @@ try {
     $pdf->Cell(40, 10, '$' . number_format($totalGeneral, 2), 1);
 
     // Salvar o enviar el PDF
-    $pdf->Output('I', 'solicitud.pdf'); // Generar PDF en pantalla
+    $pdf->Output('D', 'solicitud.pdf'); // Generar PDF en pantalla
 } catch (Exception $e) {
     echo "Error al generar PDF: " . $e->getMessage();
 }
