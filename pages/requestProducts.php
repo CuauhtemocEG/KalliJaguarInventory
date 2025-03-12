d5996202580e7ca2838e9487fc9ee788e39db564
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-11 20:21:53 -0600

    Confirm modal

--- pages/requestProducts.php
+++ pages/requestProducts.php
@@ -159,7 +159,7 @@
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="confirmModalLabel">Confirmar Solicitud a Almacén</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
                             </div>
                             <div class="modal-body">
                                 ¿Está seguro de que desea enviar la lista de productos solicitados?
