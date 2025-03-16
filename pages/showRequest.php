022b99768ca469249534e1d4bc5175743d0a8d5e
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-16 17:54:57 -0600

    Fix wrong code

--- pages/showRequest.php
+++ pages/showRequest.php
@@ -1,67 +1,169 @@
 <?php
 require_once "./controllers/mainController.php";
 
-$campos = "Productos.ProductoID,Productos.UPC,Productos.Nombre as productName,Productos.Descripcion,Productos.PrecioUnitario,Productos.Cantidad,Productos.image,Productos.CategoriaID,Productos.UsuarioID,Productos.Tipo,Categorias.CategoriaID,Categorias.Nombre as CatName,Usuarios.UsuarioID,Usuarios.Nombre,Usuarios.Username";
+$userID = $_SESSION["id"];
+$nameUser = $_SESSION["nombre"];
 
-$checkInventory = conexion();
-$checkInventory = $checkInventory->query("SELECT $campos FROM Productos INNER JOIN Categorias ON Productos.CategoriaID=Categorias.CategoriaID INNER JOIN Usuarios ON Productos.UsuarioID=Usuarios.UsuarioID");
-$datos = $checkInventory->fetchAll();
-
-$total = conexion();
-$total = $total->query("SELECT COUNT(*) FROM Productos WHERE Cantidad < 5");
-$totalCount = (int) $total->fetchColumn();
-
-$totalProd = conexion();
-$totalProd = $totalProd->query("SELECT COUNT(*) FROM Productos");
-$totalCountProd = (int) $totalProd->fetchColumn();
-
-$sucursalId='2';
-
-$dataSucursal = conexion();
-$dataSucursal = $dataSucursal->query("SELECT nombre FROM Sucursales WHERE SucursalID = '$sucursalId'");
-$nameSucursal = $dataSucursal->fetchColumn();
-
+$showComanda = conexion();
+$showComanda = $showComanda->query("SELECT MAX(FechaMovimiento) AS FechaMovimiento, ComandaID, MAX(SucursalID) AS SucursalID, MAX(MovimientoID) AS MovimientoID, COUNT(DISTINCT ProductoID) AS TotalProductos, SUM(Cantidad) AS TotalCantidad FROM MovimientosInventario WHERE TipoMovimiento = 'Salida' AND UsuarioID = $userID GROUP BY ComandaID ORDER BY MovimientoID DESC");
+$datos = $showComanda->fetchAll();
 
 ?>
 <div class="container-fluid">
-	<div class="d-flex justify-content-center row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-xl-3 col-md-6 mb-4">
-					<div class="card border-left-danger shadow h-100 py-2">
-						<div class="card-body">
-							<div class="row no-gutters align-items-center">
-								<div class="col mr-2">
-									<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-										Productos con Inventario < 0 </div>
-											<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalCount; ?></div>
-									</div>
-									<div class="col-auto">
-										<i class="fas fa-exclamation-triangle fa-2x text-gray-300"></i>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
 
-					<div class="col-xl-3 col-md-6 mb-4">
-						<div class="card border-left-success shadow h-100 py-2">
-							<div class="card-body">
-								<div class="row no-gutters align-items-center">
-									<div class="col mr-2">
-										<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-											Total de productos en Sistema</div>
-										<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalCountProd; ?></div>
-									</div>
-									<div class="col-auto">
-										<i class="fas fa-archive fa-2x text-gray-300"></i>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Mis Solicitudes abiertas </h1>
+        <a href="index.php?page=requestProducts" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-download fa-sm text-white-50"></i> Nueva Solicitud</a>
+    </div>
+
+    <div class="row">
+    <?php foreach ($datos as $row) { ?>
+
+        <?php $dataSucursal = conexion();
+            $dataSucursal = $dataSucursal->query("SELECT nombre FROM Sucursales WHERE SucursalID = ".$row['SucursalID']."");
+            $nameSucursal = $dataSucursal->fetchColumn();?>
+
+
+        <div class="col-xl-6 col-md-12 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-md-8 mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-2">
+                                fecha de la solicitud: <?php echo $row['FechaMovimiento'];?></div>
+                            <div class="h5 mb-2 font-weight-bold text-gray-800"><?php echo $row['ComandaID'];?></div>
+                            <div class="h6 mb-0 font-weight-bold text-gray-800">Cantidad de Productos:
+                            <?php echo $row['TotalProductos'];?>
+                            </div>
+                            <div class="text-xs font-weight-bold text-secondary text-uppercase mt-2">
+                                Sucursal de Destino: <?php echo $nameSucursal;?></div>
+                        </div>
+                        <div class="col mr-2">
+                            <a href="#"
+                                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                    class="fas fa-download fa-sm text-white-50"></i> Ver Solicitud</a>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><?php }?>
+
+    </div>
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Mis Solicitudes Cerradas </h1>
+    </div>
+
+    <div class="row">
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-12 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-md-8 mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                fecha de la solicitud: {{fecha}}</div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">${{Monto total}}</div>
+                            <div class="h6 mb-0 font-weight-bold text-gray-800">Cantidad de articulos:
+                                {{articulos}}
+                            </div>
+                        </div>
+                        <div class="col mr-2">
+                            <a href="#"
+                                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                    class="fas fa-download fa-sm text-white-50"></i> Ver Solicitud</a>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-12 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-md-8 mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                fecha de la solicitud: {{fecha}}</div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">${{Monto total}}</div>
+                            <div class="h6 mb-0 font-weight-bold text-gray-800">Cantidad de articulos:
+                                {{articulos}}
+                            </div>
+                        </div>
+                        <div class="col mr-2">
+                            <a href="#"
+                                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                    class="fas fa-download fa-sm text-white-50"></i> Ver Solicitud</a>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-12 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-md-8 mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                fecha de la solicitud: {{fecha}}</div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">${{Monto total}}</div>
+                            <div class="h6 mb-0 font-weight-bold text-gray-800">Cantidad de articulos:
+                                {{articulos}}
+                            </div>
+                        </div>
+                        <div class="col mr-2">
+                            <a href="#"
+                                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                    class="fas fa-download fa-sm text-white-50"></i> Ver Solicitud</a>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-12 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-md-8 mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                fecha de la solicitud: {{fecha}}</div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">${{Monto total}}</div>
+                            <div class="h6 mb-0 font-weight-bold text-gray-800">Cantidad de articulos:
+                                {{articulos}}
+                            </div>
+                        </div>
+                        <div class="col mr-2">
+                            <a href="#"
+                                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                    class="fas fa-download fa-sm text-white-50"></i> Ver Solicitud</a>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Content Row -->
+
+    </div>
 </div>
