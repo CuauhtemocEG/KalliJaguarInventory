d4220d67523abf469a36a227664bfd9f8d45c0c4
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-17 17:45:57 -0600

    Commit changes

--- pages/confirmRequest.php
+++ pages/confirmRequest.php
@@ -146,5 +146,5 @@ foreach ($_SESSION['INV'] as $item) {
 
 // Limpiar la sesión después de procesar la solicitud
 unset($_SESSION['INV']);
-echo "<script>window.setTimeout(function() { window.location = 'index.php?page=showRequest' }, 100);</script>";
-exit();
+//echo "<script>window.setTimeout(function() { window.location = 'index.php?page=showRequest' }, 100);</script>";
+//exit();
