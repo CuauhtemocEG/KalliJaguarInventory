093b20e89655c2858b0ae76b7e6c0487cbb5c991
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-17 18:19:36 -0600

    Add form for show PDF

--- pages/showPDF.php
+++ pages/showPDF.php
@@ -1,14 +1,15 @@
 <?php
 $comandaID = $_GET['ComandaID'];
-$pathPDF = './documents/'.$comandaID.'.pdf';
+$pathPDF = './documents/' . $comandaID . '.pdf';
 
 // Verifica si el archivo existe
 if (file_exists($pathPDF)) {
     // Redirigir a una página HTML que abra el archivo en una nueva ventana
     //echo '<script type="text/javascript"> window.open("' . $pathPDF . '", "_blank");</script>';
-    header('Location: ' . $pathPDF);
-    exit;
+    echo '<h1>Visualizar PDF en un Div</h1>
+<div id="pdf-container">
+    <embed src="<?php echo $pathPDF; ?>" type="application/pdf" width="100%" height="100%">
+</div>';
 } else {
     echo "El archivo no existe.";
 }
-?>
