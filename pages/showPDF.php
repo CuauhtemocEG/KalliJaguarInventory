7ebffc7e2f391563b780b4f2e572fa60cabb37d0
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-17 18:23:51 -0600

    Add container fluid

--- pages/showPDF.php
+++ pages/showPDF.php
@@ -6,10 +6,23 @@ $pathPDF = './documents/' . $comandaID . '.pdf';
 if (file_exists($pathPDF)) {
     // Redirigir a una página HTML que abra el archivo en una nueva ventana
     //echo '<script type="text/javascript"> window.open("' . $pathPDF . '", "_blank");</script>';
-    echo '<h1>Visualizar PDF en un Div</h1>
-<div id="pdf-container">
-    <embed src="'.$pathPDF.'" type="application/pdf" width="100%" height="100%">
-</div>';
+    echo '
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-6 col-md-12 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <h1>Visualizar PDF en un Div</h1>
+                            <div id="pdf-container">
+                                <embed src="'.$pathPDF
+                                .'" type="application/pdf" width="100%" height="100%">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
+     </div>';
 } else {
     echo "El archivo no existe.";
 }
