1a4f4fb1cda25fe093f2b09e1393e9b56c553edc
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-22 13:34:02 -0600

    add settings for testing

--- pages/confirmRequest.php
+++ pages/confirmRequest.php
@@ -159,7 +159,7 @@ $mail = new PHPMailer(true);
 try {
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
-    $mail->Host = 'mx128.hostgator.mx';
+    $mail->Host = 'ns128.hostgator.mx';
     $mail->SMTPAuth = true;
     $mail->Username = 'info@stagging.kallijaguar-inventory.com';
     $mail->Password = 'KalliJaguar2025@';
