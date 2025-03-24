62a61c131c5e83f18ba7693e85959ae550be8cd8
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-24 17:23:32 -0600

    edit path

--- pages/confirmRequest.php
+++ pages/confirmRequest.php
@@ -146,7 +146,7 @@ $emailUser = conexion();
 $emailUser = $emailUser->query("SELECT Email FROM Usuarios WHERE UsuarioID = '$idUser'");
 $Usermail = $emailUser->fetchColumn();
 
-require_once 'PHPMailer/PHPMailerAutoload.php';
+require './PHPMailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer(true);
 
