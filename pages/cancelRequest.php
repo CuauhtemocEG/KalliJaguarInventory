fcac3e652a8b352366ca8477e310979cd17658eb
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-27 17:52:16 -0600

    commit

--- pages/cancelRequest.php
+++ pages/cancelRequest.php
@@ -36,7 +36,7 @@ if (!isset($_GET['ComandaID'])) {
 
         $deleteComanda = conexion();
         $deleteComanda = $deleteComanda->prepare("DELETE FROM MovimientosInventario WHERE ComandaID=:id");
-        $deleteComanda->execute([":id" => $_GET['ComandaID']]);
+        $deleteComanda->execute([":id" => $comandaID]);
     }
 }
 
