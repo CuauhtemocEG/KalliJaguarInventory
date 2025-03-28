3471ae2f3f96496bf468ef1d332b85a1bca30c4c
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-03-27 18:29:40 -0600

    commit

--- pages/cancelRequest.php
+++ pages/cancelRequest.php
@@ -22,14 +22,14 @@ foreach ($datos as $item) {
     $consultStock = $consultStock->query("SELECT Cantidad as Quantity FROM Productos WHERE ProductoID='$product'");
     $stockBefore = $consultStock->fetchColumn();
 
-    $newStock = (float)$stockBefore['Quantity'] + (float)$item['Cantidad'];
+    (float)$newStock = (float)$stockBefore['Quantity'] + (float)$item['Cantidad'];
 
     try {
         $updateProducts = conexion();
         $updateProducts = $updateProducts->prepare("UPDATE Productos SET Cantidad=:stock WHERE ProductoID=:productoID");
 
         $updateProducts->execute([
-            ':stock' => (float)$newStock,
+            ':stock' => $newStock,
             ':productoID' => $item['ProductoID']
         ]);
     } catch (Exception $e) {
