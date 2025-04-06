587db7ade3e5627f02081d40baa61f7981d3c23a
CuauhtemocEG <88246306+CuauhtemocEG@users.noreply.github.com>
2025-04-06 17:04:48 -0600

    Update mainController.php

--- controllers/mainController.php
+++ controllers/mainController.php
@@ -3,7 +3,7 @@
 	# Conexion a la base de datos #
 	function conexion(){
 		$pdo = new PDO('mysql:host=localhost:3306
-;dbname=kallijag_inventory', 'kallijag_admin', 'uNtiL.horSe@5');
+;dbname=kallijag_inventory_stage', 'kallijag_stage', 'uNtiL.horSe@5');
 		return $pdo;
 	}
 
