d49b9f1420529402b72f64ae90560ea34925e5d0
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-04-23 01:19:04 -0600

    Fix to Cancel request

--- pages/showRequest.php
+++ pages/showRequest.php
@@ -73,25 +73,27 @@ $datos = $showComanda->fetchAll();
                         </div>
                     </div>
                 </div>
-            </div><?php } ?>
+            </div>
 
-    </div>
-</div>
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">¿Estas Seguro de eliminar la comanda?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">¿Estas Seguro de eliminar la comanda?</h5>
+                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">Presiona <b>"Cancelar Comanda"</b> para elminar permanentemente el registro de la base de datos (el stock regresará a estar disponible).</div>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                            <a class="btn btn-primary" href="index.php?page=cancelRequest&ComandaID=<?php echo $row['ComandaID']; ?>">Cancelar Comanda</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">Presiona <b>"Cancelar Comanda"</b> para elminar permanentemente el registro de la base de datos (el stock regresará a estar disponible).</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                <a class="btn btn-primary" href="index.php?page=cancelRequest&ComandaID=<?php echo $row['ComandaID']; ?>">Cancelar Comanda</a>
-            </div>
-        </div>
+        <?php } ?>
+
     </div>
 </div>
