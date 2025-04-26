9d048edb3fe1502505ccd0dd12cce148923a7ca6
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-04-26 01:54:17 -0600

    Add new confirm

--- js/functions.js
+++ js/functions.js
@@ -150,7 +150,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        fetch('https://stagging.kallijaguar-inventory.com/pages/confirmRequest.php', {
+        fetch('https://stagging.kallijaguar-inventory.com/index.php?page=confirmRequest.php', {
             method: 'POST',
             body: formData,
             credentials: 'include'
