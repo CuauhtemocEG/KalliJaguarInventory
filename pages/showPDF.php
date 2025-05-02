46b9451760d5a797e1357a7422e46d52df03fd13
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-05-02 14:28:50 -0600

    Add new frame

--- pages/showPDF.php
+++ pages/showPDF.php
@@ -2,27 +2,47 @@
 $comandaID = $_GET['ComandaID'];
 $pathPDF = './documents/' . $comandaID . '.pdf';
 
-// Verifica si el archivo existe
 if (file_exists($pathPDF)) {
-    // Redirigir a una página HTML que abra el archivo en una nueva ventana
-    //echo '<script type="text/javascript"> window.open("' . $pathPDF . '", "_blank");</script>';
     echo '
+    <style>
+        #pdf-container {
+            width: 100%;
+            height: 90vh;
+        }
+
+        #pdf-container iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+
+        @media (max-width: 768px) {
+            #pdf-container {
+                height: 85vh;
+            }
+        }
+
+        h3 {
+            font-size: 1.2rem;
+            text-align: center;
+            margin-bottom: 1rem;
+        }
+    </style>
+
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
-                        <h3>'.$comandaID.'</h3>
-                            <div id="pdf-container">
-                                <embed src="'.$pathPDF
-                                .'" type="application/pdf" width="100%" height="800px">
-                            </div>
+                        <h3>' . htmlspecialchars($comandaID) . '</h3>
+                        <div id="pdf-container">
+                            <iframe src="' . $pathPDF . '#toolbar=1&navpanes=1&scrollbar=1"></iframe>
                         </div>
                     </div>
                 </div>
             </div>
-         </div>
-     </div>';
+        </div>
+    </div>';
 } else {
-    echo "El archivo no existe.";
+    echo "<div class='alert alert-danger'>El archivo no existe.</div>";
 }
