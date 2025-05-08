bd418da7e993d372ff56cb63b96c0eec183e8c78
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-05-08 14:34:15 -0600

    Fix to quantity in logic to weight

--- js/productsRequested/searchProducts.php
+++ js/productsRequested/searchProducts.php
@@ -26,7 +26,7 @@ foreach ($datos as $row) {
 
     if ($row['Cantidad'] >= 1 && $row['Tipo'] == 'Unidad' ) {
         $txtDisponibilidad = '<span class="badge badge-success">Disponible</span>';
-    } elseif ($row['Cantidad'] >= 0.1 && $row['Tipo'] == 'Pesable') {
+    } elseif ($row['Cantidad'] > 0 && $row['Tipo'] == 'Pesable') {
         $txtDisponibilidad = '<span class="badge badge-success">Disponible</span>';
     } else {
         $txtDisponibilidad = '<span class="badge badge-danger">No disponible</span>';
