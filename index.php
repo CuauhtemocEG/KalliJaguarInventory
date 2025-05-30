eb0241fc413d207311c85935be89a52909da6798
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-05-29 22:07:28 -0600

    Add library qrcode

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ session_start(); ?>
     <meta name="author" content="">
     <title>Kalli Jaguar Inventory</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <script src="https://unpkg.com/html5-qrcode"></script>
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
