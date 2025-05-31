c03df256aece84a5b74a556184176cbf64f176be
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-05-30 18:46:34 -0600

    Delete savecode.php to proyect

--- js/scanProducts/saveCode.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["codigo"])) {
-    $codigo = $_POST["codigo"];
-    header("Location: index.php?page=updateStockProduct&codigo=" . urlencode($codigo));
-    exit();
-}
