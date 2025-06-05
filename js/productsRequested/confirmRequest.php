a47ee7da4d3e965f9f7b046abd9916fe55540cea
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-06-05 01:14:19 -0600

    Add debug errors

--- js/productsRequested/confirmRequest.php
+++ js/productsRequested/confirmRequest.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $requiredFiles = [
   '../../fpdf/fpdf.php',
   '../../controllers/mainController.php',
