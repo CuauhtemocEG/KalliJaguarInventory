c0433a6ee420c21de3ff3b747b7051f0e13b16e6
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-06-05 01:17:06 -0600

    delete debug

--- js/productsRequested/confirmRequest.php
+++ js/productsRequested/confirmRequest.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 $requiredFiles = [
   '../../fpdf/fpdf.php',
   '../../controllers/mainController.php',
