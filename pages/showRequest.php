23063b76fc1c5294a1839d23cc9c65a796b8fffe
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-06-15 22:17:57 -0600

    delete start session

--- pages/showRequest.php
+++ pages/showRequest.php
@@ -1,10 +1,5 @@
 <?php
 require_once "./controllers/mainController.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-session_start();
 $userID = $_SESSION["id"];
 $nameUser = $_SESSION["nombre"];
 
