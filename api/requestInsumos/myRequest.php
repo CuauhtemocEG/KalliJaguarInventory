f0a71cba591b26e1730b731884d14ebe0c311ae6
CuauhtemocEG <temoc612@gmail.com>
2025-07-26 17:23:44 -0600

    add user

--- api/requestInsumos/myRequest.php
+++ api/requestInsumos/myRequest.php
@@ -18,11 +18,13 @@ $stmt = $conn->prepare("SELECT
     S.nombre AS SucursalNombre,
     MAX(MI.MovimientoID) AS MovimientoID, 
     COUNT(DISTINCT MI.ProductoID) AS TotalProductos, 
-    SUM(MI.Cantidad) AS TotalCantidad 
+    SUM(MI.Cantidad) AS TotalCantidad,
+    U.Nombre AS UsuarioNombre
 FROM MovimientosInventario MI
 INNER JOIN Sucursales S ON S.SucursalID = MI.SucursalID
+INNER JOIN Usuarios U ON U.UsuarioID = MI.UsuarioID
 WHERE MI.TipoMovimiento = 'Salida' AND MI.UsuarioID = ?
-GROUP BY MI.ComandaID, MI.Status, MI.UsuarioID, S.nombre
+GROUP BY MI.ComandaID, MI.Status, MI.UsuarioID, S.nombre, U.Nombre
 ORDER BY MovimientoID DESC
                         ");
 $stmt->execute([$userId]);
