d7589abfeb00b59f8e8ef85d68a795f56fd3dda6
CuauhtemocEG <temoc612@gmail.com>
2025-07-26 15:43:42 -0600

    add new fields

--- api/requestInsumos/searchProducts.php
+++ api/requestInsumos/searchProducts.php
@@ -5,10 +5,20 @@ require_once '../../controllers/mainController.php';
 $query = isset($_GET['query']) ? $_GET['query'] : '';
 $conn = conexion();
 
-$sql = "SELECT ProductoID, UPC, Nombre, Descripcion, PrecioUnitario, Cantidad, Tipo, CategoriaID, image 
-        FROM Productos 
-        WHERE Nombre LIKE :query OR UPC LIKE :query 
-        ORDER BY Nombre";
+$sql = "SELECT 
+            p.ProductoID, 
+            p.UPC, 
+            p.Nombre, 
+            p.Descripcion, 
+            p.PrecioUnitario, 
+            p.Cantidad, 
+            p.Tipo, 
+            c.Nombre AS nombreCategoria, 
+            p.image 
+        FROM Productos p
+        INNER JOIN Categorias c ON p.CategoriaID = c.CategoriaID
+        WHERE p.Nombre LIKE :query OR p.UPC LIKE :query 
+        ORDER BY p.Nombre";
 $stmt = $conn->prepare($sql);
 $stmt->execute([':query' => "%$query%"]);
 $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
