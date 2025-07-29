55f5c384f1510a931c7b9d096e9ea72c2a023f70
CuauhtemocEG <temoc612@gmail.com>
2025-07-28 22:35:34 -0600

    fix and add files

--- api/comandaDetails/cancelRequest.php
+++ api/comandaDetails/cancelRequest.php
@@ -43,9 +43,9 @@ $emailUser = conexion();
 $emailUser = $emailUser->query("SELECT Email FROM Usuarios WHERE UsuarioID = '$idUser'");
 $Usermail = $emailUser->fetchColumn();
 
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
-require './PHPMailer/src/Exception.php';
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
+require '../../PHPMailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
