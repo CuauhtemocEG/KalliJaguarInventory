bcda552f3e43a581c2e32cd42f1fafe40c37281d
CuauhtemocEG <temoc612@gmail.com>
2025-09-17 02:38:56 -0600

    changes to bgray-50 to-blue-50 dark:from-gray-900 dark:to-gray-800

--- pages/editarComanda.php
+++ pages/editarComanda.php
@@ -55,7 +55,7 @@
         }
     </style>
 
-    <body class="bg-gradient-to-br from-gray-50 to-blue-50 dark:from-gray-900 dark:to-gray-800 min-h-screen">
+    <body class="bg-white dark:bg-gray-900 min-h-screen">
         <div class="bg-white dark:bg-gray-800 shadow-sm border-b border-gray-200 dark:border-gray-700">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                 <div class="flex items-center justify-between">
