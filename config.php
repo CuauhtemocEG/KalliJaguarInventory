aa7fdbc57562708783fdcff734a124aa080966b6
CuauhtemocEG <temoc612@gmail.com>
2025-12-13 20:30:07 -0600

    edit new versión

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 
 define('APP_NAME', 'Kalli Inventory System');
 define('APP_SHORT_NAME', 'Kalli Jaguar');
-define('APP_VERSION', '2.1.2');
+define('APP_VERSION', '2.1.8');
 define('APP_BUILD', '20250415'); // Formato: YYYYMMDD
 define('APP_RELEASE_DATE', '9 de diciembre de 2025');
 
